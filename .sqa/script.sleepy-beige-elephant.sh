ae45f15c9f2dec5820af5f085c9e87addb888ced
PalomoIFCA <143794185+PalomoIFCA@users.noreply.github.com>
2025-03-14 08:47:39 +0100

    Add JePL file structure

--- .sqa/script.sleepy-beige-elephant.sh
+++ .sqa/script.sleepy-beige-elephant.sh
@@ -5,5 +5,5 @@
 
 (
 cd github.com/OphidiaBigData/ophidia-workflow-catalogue &&
-    find_oph_workflows.py --path . --args [{"lss .json":["1holistical"]}]
+    find_oph_workflows.py --path . --args {"lss.json":["1" "historic"]}
 )
